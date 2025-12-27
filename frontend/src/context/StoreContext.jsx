42047439a00f174945dd3046518eabd4cdc315cb
kunal171003 <kunalbhosale650@gmail.com>
2025-12-27 19:46:23 +0530

    Update StoreContent.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url ="http://localhost:4000"
+  const url ="https://foodos-a-food-delivey-app.onrender.com"
   const [token,setToken] = useState("")
   const [food_list,setFoodList] = useState([]);
 
